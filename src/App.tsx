2bfe77e474e85f0b735df090c33d18d547ab348d
johnpham <pltnhan311@gmail.com>
2024-04-09 23:55:26 +0700

    update 1, to create api

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import './App.css'
+import AuthLayout from './_auth/AuthLayout'
+import SigninForm from './_auth/forms/SigninForm'
+import SignupForm from './_auth/forms/SignupForm'
+import RootLayout from './_root/RootLayout'
+import { Home } from './_root/pages'
+import './global.css'
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <main className='flex h-screen'>
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path='/sign-in' element={<SigninForm />} />
+          <Route path='/sign-up' element={<SignupForm />} />
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  )
+}
+
+export default App
